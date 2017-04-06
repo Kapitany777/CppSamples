c6385ebfc548ea04dd948c07591b1c0d3b784d3a
Kapitany777 <torokv@vnet.hu>
2017-04-06 20:23:00 +0200

    P03Arithmetic

---
+++ P03Arithmetic/arithmetic.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "5 + 4 = " << 5 + 4 << endl;
+	cout << "5 - 4 = " << 5 - 4 << endl;
+	cout << "5 * 4 = " << 5 * 4 << endl;
+	cout << "5 / 4 = " << 5 / 4 << endl;
+	cout << "5 % 4 = " << 5 % 4 << endl;
+
+	cout << "5 + 4 * 8 = " << 5 + 4 * 8 << endl;
+	cout << "(5 + 4) * 8 = " << (5 + 4) * 8 << endl;
+	
+	return 0;
+}
